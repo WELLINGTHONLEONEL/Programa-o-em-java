1b48ac3db6dfddf363842722bc8886b96ce94a4d
WELLINGTHONLEONEL <160232898+WELLINGTHONLEONEL@users.noreply.github.com>
2024-07-26 12:05:04 -0300

    c1

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-alert('Boas vindas ao jogo do número secreto');
+alert('Bem vindo lek, vamo ver se você presta pra algo');
 let numeroSecreto = 5;
 console.log('numeroSecreto')
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute = prompt('Escolha um número entre 1 e 100');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (5)');
+alert('Não fez mais q a sua obrigação! paura béns (24)');
 } else {
-alert('Você errou :(')
+alert('Ruim demais, presta pra nada memo seu burro >:[')
 }
